3c3af69fb3721d4d3bfeedaa8ec2a65fb65cdb46
Andy <Andy@Andy-PC>
2012-11-27 17:10:28 +0100

    Try to make a branch and so on

--- TranslateAmbiguities/src/engine/TranslationProcessor.java
+++ TranslateAmbiguities/src/engine/TranslationProcessor.java
@@ -1,8 +1,13 @@
 package engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import common.Corpus;
 
 import entities.Text;
+import entities.TranslationEntry;
+import entities.Translations;
 
 public class TranslationProcessor {
 
@@ -10,7 +15,33 @@ public class TranslationProcessor {
 	Corpus corpus;
 	
 	
-	public void translateWord(String wordToTranslate){
+	public String translateWord(String wordToTranslate, int sentenceIndex){
+		Translations translations = corpus.getTranslations(wordToTranslate);
 		
+		List<Integer> points = new ArrayList<Integer>();	//List to hold the points of the possible translation
+		
+		if(translations == null){
+			return wordToTranslate+"-NOTFOUND";
+		}
+		
+		List<TranslationEntry> entrys = translations.getEntrys();
+		if(entrys.size() == 1){
+			return entrys.get(0).getTranslatedWord();
+		}
+		
+		//calculate points
+		for(TranslationEntry entry : entrys){
+			points.add(calculatePoints(entry, sentenceIndex));
+		}
+		
+		return null;
+		
+	}
+
+
+	private Integer calculatePoints(TranslationEntry entry, int sentenceIndex) {
+		Integer amountOfPoints = new Integer(0);
+		
+		return null;
 	}
 }
